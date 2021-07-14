171963ac45d60a8aa5139ae3a31f2d08ce466e2d
kartavya-p <42864275+kartavya-p@users.noreply.github.com>
2021-07-14 15:15:20 +0530

    Add files via upload

---
+++ Assignment3/Class.js
@@ -0,0 +1,6 @@
+function getdata() 
+{
+    var Name = document.getElementById('name');
+    var Value = Name.value;
+    document.getElementById('page').innerText = " Hello!! " + Value + " welcome to our page  ";
+}
